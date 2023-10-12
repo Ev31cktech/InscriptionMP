e96e197b4f4a129138b66049b3dbfa4bdbfbd132
Ev31cktech <goejevraag@mebal.com>
2023-10-12 09:49:10 +0200

    Act1 card pack updated with portraits and bone and animal card types added

---
+++ Act1 CardPack/AnimalCardData.cs
@@ -0,0 +1,20 @@
+﻿using Inscryption_Server.DataTypes;
+
+namespace Act1_CardPack.Cards
+{
+	public class AnimalCardData : CardData
+	{
+		public string ElderlyID { get; set; }
+		public string BabyID { get; set; }
+		public AnimalCardData()
+		{}
+
+		public override bool CanPlay(CardData card)
+		{
+			return false;
+		}
+
+		public override void PlayCard()
+		{}
+	}
+}
---
+++ Act1 CardPack/BoneCardData.cs
@@ -0,0 +1,24 @@
+﻿using Inscryption_Server.DataTypes;
+using System.Data;
+using System.Diagnostics.Contracts;
+using System.Runtime.Remoting.Messaging;
+
+namespace Act1_CardPack
+{
+	public class BoneCard : Card
+	{
+		public static uint BoneCount { get; } = 0;
+		public BoneCard(CardData cardData) : base(cardData)
+		{
+		}
+
+		public override bool CanPlay(CardData boneCard)
+		{
+			return boneCard.Cost.Amount <= BoneCount;
+		}
+
+		public override void OnPlay()
+		{
+		}
+	}
+}
